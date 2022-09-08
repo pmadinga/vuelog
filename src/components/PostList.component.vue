5ff9128f66a25d40a2a9a5f12b8752a27761551d
Phiwa Madinga <phiwa@koedr.org>
2022-09-08 15:20:35 +0200

    init

---
+++ src/components/PostList.component.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="post-list">
+    <div v-for="(post, index) in posts" :key="post.id">
+      <SinglePostComponent :post="post"/>
+      
+    </div>
+  </div>
+</template>
+<script>
+import { onMounted, onUpdated } from 'vue';
+import SinglePostComponent from './SinglePost.component.vue';
+  export default{
+    props: ["posts"],
+    components: { SinglePostComponent },
+    setup(props) {
+      onMounted(() => console.log('mounted'))
+      onUpdated(() => console.log('updated'))
+    },
+}
+</script>
+
+<style>
+
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home</h1>
+    <PostListComponent v-if="showPosts" :posts="posts"/>
+    <button @click="showPosts = !showPosts">toggle posts</button>
+    <button @click="posts.pop()">delete a post</button>
   </div>
+  
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import { computed, reactive, ref, watch, watchEffect } from 'vue'
+import PostListComponent from '../components/PostList.component.vue'
+
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+    name: "HomeView",
+    components: { PostListComponent },
+    setup() {
+        const posts = ref([
+            { title: "Welcome to the blog", body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque iure optio, consequatur cupiditate aperiam iste voluptatum incidunt, ducimus maxime veniam voluptates tempora nulla, dolor doloribus similique laborum velit fuga error?", id: 1 },
+            { title: "Top 5 css", body: "lorem ipsum", id: 2 }
+        ]);
+        const showPosts = ref(true)
+        return { posts, showPosts }
+    },
 }
 </script>
